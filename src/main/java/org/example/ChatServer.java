2ff812504c44e6af4721bf7833594f3e1af06eba
ztype764 <49955693+ztype764@users.noreply.github.com>
2024-07-06 21:30:56 +0530

    Remove user when disconnecting, remove stacktrace on server

--- src/main/java/org/example/ChatServer.java
+++ src/main/java/org/example/ChatServer.java
@@ -1,10 +1,14 @@
 package org.example;
 
-import javax.crypto.SecretKey;
-import java.io.*;
-import java.net.*;
-import java.util.*;
-import java.util.logging.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ChatServer {
     private static final Logger logger = Logger.getLogger(ChatServer.class.getName());
@@ -57,81 +61,6 @@ public class ChatServer {
         clientNames.add(name);
     }
 }
-
-class ClientHandler implements Runnable {
-    private static final Logger logger = Logger.getLogger(ClientHandler.class.getName());
-    private final Socket socket;
-    private PrintWriter out;
-    private String clientName;
-
-    public ClientHandler(Socket socket) {
-        this.socket = socket;
-    }
-
-    @Override
-    public void run() {
-        try {
-            out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-            // Read the client's name and check if it's available
-            while (true) {
-                clientName = in.readLine();
-                if (ChatServer.isNameAvailable(clientName)) {
-                    ChatServer.addClientName(clientName);
-                    out.println("OK");
-                    break;
-                } else {
-                    out.println("Name already taken");
-                }
-            }
-
-            logger.info(clientName + " has joined the chat");
-            ChatServer.broadcastMessage(clientName + " has joined the chat", this);
-            ChatServer.clientHandlers.add(this);
-
-            String message;
-            while ((message = in.readLine()) != null) {
-                try {
-                    long ntpTime = CryptoUtils.getNTPTime() / 1000;
-                    SecretKey key = CryptoUtils.deriveKey( ntpTime);
-                    String decryptedMessage = CryptoUtils.decrypt(message, key);
-                    ChatServer.broadcastMessage("[" + clientName + "]" + ": " + decryptedMessage, this);
-                    System.out.println("[" + clientName + "]" + ": " + decryptedMessage);
-                } catch (Exception e) {
-                    logger.log(Level.SEVERE, "Error decrypting message", e);
-                }
-            }
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, "Error in client handler", e);
-        } finally {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                logger.log(Level.INFO, "Error closing socket", e);
-            }
-            ChatServer.removeClient(this);
-            logger.info(clientName + " has left the chat");
-            ChatServer.broadcastMessage(clientName + " has left the chat", this);
-        }
-    }
-
-    public void sendMessage(String message) {
-        try {
-            long ntpTime = CryptoUtils.getNTPTime() / 1000;
-            SecretKey key = CryptoUtils.deriveKey(ntpTime);
-            String encryptedMessage = CryptoUtils.encrypt(message, key);
-            out.println(encryptedMessage);
-        } catch (Exception e) {
-            logger.log(Level.SEVERE, "Error encrypting message", e);
-        }
-    }
-
-    public String getClientName() {
-        return clientName;
-    }
-}
-
 class ServerInputHandler implements Runnable {
     private static final Logger logger = Logger.getLogger(ServerInputHandler.class.getName());
     private final BufferedReader stdIn;
